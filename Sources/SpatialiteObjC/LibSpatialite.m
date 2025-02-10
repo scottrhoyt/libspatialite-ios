9ae08bcc5b1cf59c19661f916e9d60db237c072b
Julian Lo <julian@mozi.app>
2025-02-10 10:33:32 -0800

    Fix setupSpatialite

--- Sources/SpatialiteObjC/LibSpatialite.m
+++ Sources/SpatialiteObjC/LibSpatialite.m
@@ -1,7 +1,10 @@
 #import <Foundation/Foundation.h>
 #import "include/LibSpatialite.h"
 
-void setupSpatialite(){
-  spatialite_init(0);
-  puts("SpatiaLite integrated");
+void setupSpatialite(sqlite3 *db_handle) {
+  const void *ptr;
+  void *cache;
+  cache = spatialite_alloc_connection();
+  int verbose = 1;
+  spatialite_init_ex (db_handle, cache, verbose);
 }
