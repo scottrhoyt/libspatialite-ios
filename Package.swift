72a6fcf0f2063f8b1732ac6e4728210fead4cb44
Scott Hoyt <scottrhoyt@gmail.com>
2025-10-03 09:32:36 -0700

    Bump framework download to v0.2.2

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "libspatialite",
-      url: "https://github.com/scottrhoyt/libspatialite-ios/releases/download/v0.2.1/libspatialite.xcframework.zip",
-      checksum: "bb208896a05aab3742e8a8ad6a623b8d3423747b93753cb06a9da0db558c9008"
+      url: "https://github.com/scottrhoyt/libspatialite-ios/releases/download/v0.2.2/libspatialite.xcframework.zip",
+      checksum: "38b7ddb0df4d799e1458b948771185c94c69378e2cad2f2e866cdf4d5aa294ae"
     ),
     .target(
       name: "SpatialiteObjC",
